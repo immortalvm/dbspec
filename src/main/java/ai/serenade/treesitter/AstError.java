e3450cbc900f8b9b357ccb020bbd02676c283366
Thor Kristoffersen <thor@nr.no>
2023-04-19 16:21:39 +0200

    Remove comment

--- src/main/java/ai/serenade/treesitter/AstError.java
+++ src/main/java/ai/serenade/treesitter/AstError.java
@@ -1,10 +1,5 @@
 package ai.serenade.treesitter;
 
-/**
- * In theory, this error should never be thrown. If it does get thrown, it
- * indicates a mismatch between the parser and the interpreter, so it may be
- * useful for debugging purposes.
- */
 public class AstError extends Error {
 	private static final long serialVersionUID = 1L;
 }
