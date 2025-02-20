6d9f1696ab2a7067dc947a90ff0859996f9946c3
Ivar Rummelhoff <ivar.rummelhoff@nr.no>
2025-02-20 12:03:12 +0100

    .roae output quick fix
    NB. Since this is not very robust w.r.t. title string, we should probably
    disallow titles with newlines or " for now.

--- src/main/java/no/nr/dbspec/RoaeProducerImpl.java
+++ src/main/java/no/nr/dbspec/RoaeProducerImpl.java
@@ -13,15 +13,20 @@ public class RoaeProducerImpl implements RoaeProducer {
     public void updateMetadata(Path path, List<RoaeMd> commands) throws IOException {
         PrintWriter output = new PrintWriter(new FileWriter(path.toFile()));
         for (RoaeMd cObj : commands) {
-            output.write(cObj.getData());
-            output.format("%s\n", cObj.getData());
+            output.format("Command:\n");
+            output.format("\ttitle = \"%s\"\n", cObj.getData());
+            output.format("\tParameters:\n");
             for (RoaeMd pObj : cObj.getChildren(RoaeMdType.PARAMETER)) {
                 String description = pObj.getData();
-                output.format("%s - %s\n", pObj.getName(), description == null ? "" : description);
+                output.format("\t\t%s - %s\n", pObj.getName(), description == null ? "" : description);
             }
+            output.format("\tBody:\n");
             for (RoaeMd sObj : cObj.getChildren(RoaeMdType.SQL)) {
-                output.format("%s\n", sObj.getData());
+                for (String line : sObj.getData().split("\n")) {
+                    output.format("\t\t%s\n", line);
+                }
             }
+            output.format("\n");
         }
         output.close();
     }
